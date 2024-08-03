71f8de5c358db9231e8f485dc305352e31651011
newnhcmmkk1 <githubn434@gmail.com>
2024-08-03 20:05:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
+  OWNER_NUMBER: "94778764083,
+  SUDO_NUMBERS : "94766969406,94778764083",
+  OWNER_NAME: "『 MR MIHIRUK 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
